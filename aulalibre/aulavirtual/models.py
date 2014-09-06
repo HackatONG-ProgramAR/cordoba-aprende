0e49d0d66f03dfd5a451573b41064d0067946cd7
Francisco J. Capdevila <fjcapdevila@gmail.com>
2014-09-06 14:36:00 -0300

    Algunos modelos mas

--- aulalibre/aulavirtual/models.py
+++ aulalibre/aulavirtual/models.py
@@ -1,5 +1,5 @@
+# -*- coding: utf-8 -*-
 from django.db import models
-
 
 # Create your models here.
 
@@ -17,7 +17,6 @@ class Alumno(models.Model):
 
     def __unicode__(self):
         return "%s, %s" % (self.apellido, self.nombre)
-
 
 
 class Profesor(models.Model):
@@ -57,3 +56,80 @@ class Curso(models.Model):
 
     def __unicode__(self):
         return "%s, %s" % (self.anio, self.division)
+
+
+class CursoMateria(models.Model):
+    curso = models.ForeignKey('Curso')
+    profesor = models.ForeignKey('Profesor')
+    anio_materia = models.ForeignKey('AnioMateria')
+
+    class Meta:
+        verbose_name = ('CursoMateria')
+        verbose_name_plural = ('CursoMateria')
+
+    def __unicode__(self):
+        return "%s, %s" % (self.anio, self.division)
+
+
+class AnioMateria(models.Model):
+    materia = models.ForeignKey('Materia')
+    anio = models.IntegerField(verbose_name='Año')
+
+    class Meta:
+        verbose_name = ('AnioMateria')
+        verbose_name_plural = ('AnioMateria')
+
+    def __unicode__(self):
+        return "%s, %s" % (self.anio, self.materia)
+
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=100)
+    area_tematica = models.ForeignKey('AreaTematica')
+
+    class Meta:
+        verbose_name = ('AnioMateria')
+        verbose_name_plural = ('AnioMateria')
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class AreaTematica(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = ('Area Tematica')
+        verbose_name_plural = ('Areas Tematicas')
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class Eje(models.Model):
+    nombre = models.CharField(max_length=100)
+    anio_materia = models.ForeignKey('AnioMateria')
+    # contenidos
+
+    class Meta:
+        verbose_name = ('Eje')
+        verbose_name_plural = ('Ejes')
+
+    def __unicode__(self):
+        return self.nombre
+
+
+class Recurso(models.Model):
+    tipo = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    adjunto = models.FileField(upload_to='recursos')
+    area_tematica = models.ForeignKey('AreaTematica')
+    anio = models.IntegerField(verbose_name='Año')
+
+    class Meta:
+        verbose_name = ('Eje')
+        verbose_name_plural = ('Ejes')
+
+    def __unicode__(self):
+        return self.nombre
