8cac0674ccc710f94f86439888da4a071c2c8cc5
Francisco J. Capdevila <fjcapdevila@gmail.com>
2014-09-06 16:14:10 -0300

    Mejoras en el Admin

--- aulalibre/aulavirtual/admin.py
+++ aulalibre/aulavirtual/admin.py
@@ -6,12 +6,36 @@ from aulavirtual.models import (
 
 # Register your models here.
 
-admin.site.register(Alumno)
+
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ['apellido', 'nombre', 'curso']
+    search_fields = ('nombre', 'apellido')
+    list_filter = ['curso', ]
+
+
+class AnioMateriaAdmin(admin.ModelAdmin):
+    list_display = ['materia', 'anio']
+    search_fields = ('materia', )
+    list_filter = ['materia', 'anio']
+
+
+class ColegioAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'ciudad']
+    search_fields = ['nombre', 'ciudad']
+    list_filter = ['ciudad']
+
+
+class CursoMateriaAdmin(admin.ModelAdmin):
+    list_display = ['anio_materia', 'profesor', 'curso']
+    search_fields = ['anio_materia', 'profesor', 'curso']
+    list_filter = ['anio_materia', 'profesor', 'curso']
+
+admin.site.register(Alumno, AlumnoAdmin)
 admin.site.register(Profesor)
-admin.site.register(Colegio)
+admin.site.register(Colegio, ColegioAdmin)
 admin.site.register(Curso)
-admin.site.register(CursoMateria)
-admin.site.register(AnioMateria)
+admin.site.register(CursoMateria, CursoMateriaAdmin)
+admin.site.register(AnioMateria, AnioMateriaAdmin)
 admin.site.register(Materia)
 admin.site.register(AreaTematica)
 admin.site.register(Eje)
